4c6cf46f3e50bd7dc6c64dc974228b8a4f9ed461
luis martinez <lushiru@hotmail.com>
2025-05-24 10:45:07 -0400

    stylo

--- src/screens/Veragenda/VerAgendaScreen/VerAgendaScreen.styles.js
+++ src/screens/Veragenda/VerAgendaScreen/VerAgendaScreen.styles.js
@@ -11,4 +11,16 @@ export const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 10
   },
+  btnSubmit:{
+    padding: 5,
+    fontSize: 16,
+    backgroundColor: "#254ffd",
+    margin:10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
